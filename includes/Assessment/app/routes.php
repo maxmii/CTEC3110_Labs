52e2d85b72ca00f978ef5d8c0933e356c4114eba
Max Morris <p15213515@my365.dmu.ac.uk>
2018-12-05 11:42:20 +0000

    Moved app to the includes directory as it needs to be private

--- includes/Assessment/app/routes.php
+++ includes/Assessment/app/routes.php
@@ -6,4 +6,4 @@
  * Time: 17:17
  */
 
-require 'routes/homepage.php';
+require 'routes/Homepage.php';
--- public_php/Assessment/public/index.php
+++ public_php/Assessment/public/index.php
@@ -11,14 +11,14 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require 'vendor/autoload.php';
 
-$settings = require __DIR__ . '/../app/settings.php';
+$settings = require  'Assessment/app/settings.php';
 
 $container = new \Slim\Container($settings);
 
-require __DIR__ . '/../app/dependencies.php';
+require 'Assessment/app/dependencies.php';
 
 $app = new \Slim\App($container);
 
-require __DIR__ . '/../app/routes.php';
+require 'Assessment/app/routes.php';
 
 $app->run();
